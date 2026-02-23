c2e353155e1be2b875870cc4af45d9bc3ae1f96c
jessec01 <jegro@gmail.com>
2026-02-23 10:15:28 -0400

    feat: creacion del crud para el modelos instructor

--- instructor/serializer.py
+++ instructor/serializer.py
@@ -1,12 +1,19 @@
+#instrcutor/serializer.py
+# importaciones estandar de python
 import re 
 
+# importaciones de terceros
+from django.db import transaction
 from rest_framework import serializers
+
+# importaciones de models
 from instructor.models import Instructor
+from user.models import User    
 class InstructorSerializer(serializers.ModelSerializer):
+    """Serializer para crear un instructor"""
     class Meta:
         model = Instructor
         fields = ['user','at_creation','especiality','photo_profile','description'] 
-        ready_only_fields = ['user','at_creation']
     def validate_especiality(self,value_especiality):
         especiality=re.search('^[a-zA-Z\s]{4,100}$',value_especiality)
         if not especiality:
@@ -26,15 +33,55 @@ class InstructorSerializer(serializers.ModelSerializer):
         especiality=validated_data.get('especiality')
         description=validated_data.get('description')
         photo_profile=validated_data.get('photo_profile')
-        instructor=Instructor.objects.create(
-            user=user,
-            especiality=especiality,
-            description=description,
-            photo_profile=photo_profile
+        #valida internamente el user.required
+        with transaction.atomic():
+            instructor=Instructor.objects.create(
+                user=user,
+                especiality=especiality,
+                description=description,
+                photo_profile=photo_profile
         )
         return instructor
-    
+class InstructorUpdateSerializer(serializers.ModelSerializer):
+    """ Serializer para actualizar un instructor """
+    class Meta:
+        model = Instructor
+        fields = ['especiality','photo_profile','description'] 
+    def validate_especiality(self,value_especiality):
+        especiality=re.search('^[a-zA-Z\s]{4,100}$',value_especiality)
+        if not especiality:
+            raise serializers.ValidationError({"especiality": "invalid especiality"})
+        return value_especiality
+    def validate_description(self,value_description):
+        description=re.search('^[a-zA-Z\s]{4,500}$',value_description)
+        if not description:
+            raise serializers.ValidationError({"description": "invalid description"})
+        return value_description
+    def validate_photo_profile(self, value_photo_profile):
+        if value_photo_profile and not value_photo_profile.name.lower().endswith(('.jpg', '.jpeg', '.png')):
+            raise serializers.ValidationError({"photo_profile": "Invalid file type. Only .jpg, .jpeg, and .png are allowed."})
+        return value_photo_profile   
+class InstructorListSerializer(serializers.ModelSerializer):
+    """ Serializer para listar un instructor """
+    first_name=serializers.CharField(source='user.first_name',read_only=True)
+    last_name=serializers.CharField(source='user.last_name',read_only=True)
+    class Meta:
+        model=Instructor
+        fields=['first_name','last_name','especiality','photo_profile','description']
+class UserSerializer(serializers.ModelSerializer):
+    """ Serializer para mostrar un usuario """
+    class Meta:
+        model=User
+        fields=['first_name','last_name','email']
+
+class DetailInstructorSerializer(serializers.ModelSerializer):
+    """ Serializer para mostrar un instructor """
+    user=UserSerializer(read_only=True)
+    class Meta:
+        model=Instructor
+        fields=['user','especiality','photo_profile','description']
 class LoginSerializer(serializers.Serializer):
+    """ Serializer para iniciar sesión """
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)   
     def validate_email(self, value):
@@ -47,5 +94,10 @@ class LoginSerializer(serializers.Serializer):
             raise serializers.ValidationError({"password": "Password is required."})
         return value
     def validate(self, attrs):
-        return super().validate(attrs)      
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(email=email, password=password)
+        if not user:
+            raise serializers.ValidationError({"email": "Invalid email or password."})
+        return attrs      
     
